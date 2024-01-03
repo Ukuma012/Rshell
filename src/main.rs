c5d6d87716234a9cd339296f4db3220f914b66d7
Ukuma012 <Ukuma012@gmail.com>
2024-01-03 23:59:06 +0900

    delete comment

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,22 @@ use std::process::{Child, Command, Stdio};
 
 fn main() {
     loop {
-        // use the `>` character as the prompt
-        // need to explicitly flush this to ensure it prints before read_line
         print!("> ");
         stdout().flush().unwrap();
 
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        // read_line leaves a trailing newline, which trim removes
-        // this needs to be peekable so we can determine when we are on the last command
         let mut commands = input.trim().split(" | ").peekable();
         let mut previous_command = None;
 
         while let Some(command) = commands.next() {
-            // everything after the first whitespace character is interpreted as args to the command
             let mut parts = command.trim().split_whitespace();
             let command = parts.next().unwrap();
             let args = parts;
 
             match command {
                 "cd" => {
-                    // default to '/' as new directory if one was not provided
                     let new_dir = args.peekable().peek().map_or("/", |x| *x);
                     let root = Path::new(new_dir);
                     if let Err(e) = env::set_current_dir(&root) {
@@ -42,12 +36,8 @@ fn main() {
                     });
 
                     let stdout = if commands.peek().is_some() {
-                        // there is another command piped behind this one
-                        // prepare to send output to the next command
                         Stdio::piped()
                     } else {
-                        // there are no more commands piped behind this one
-                        // send output to shell stdout
                         Stdio::inherit()
                     };
 
@@ -71,7 +61,6 @@ fn main() {
         }
 
         if let Some(mut final_command) = previous_command {
-            // block until the final command has finished
             final_command.wait().unwrap();
         }
     }
